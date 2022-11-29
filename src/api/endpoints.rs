dcf404886d741777380bf313be258607747cf7ae
fdinsen <gammarik@gmail.com>
2022-11-29 14:09:47 +0100

    Adds generic err response function

--- src/api/endpoints.rs
+++ src/api/endpoints.rs
@@ -1,6 +1,7 @@
 use crate::{models::orders::CreateOrder};
-use super::utils::env;
-use actix_web::{get, post, HttpResponse, Responder, web};
+use super::utils::env::{get_db_ip};
+use actix_web::{get, post, HttpResponse, Responder, web, HttpResponseBuilder};
+use serde::Serialize;
 use super::workers;
 const DB_IP: &str = "165.22.194.124:9090";
 
@@ -8,13 +9,9 @@ const DB_IP: &str = "165.22.194.124:9090";
 pub async fn create(param_obj: web::Json<CreateOrder>) -> impl Responder {
     match workers::create_order(param_obj, &DB_IP) {
         Ok(r) => 
-            return HttpResponse::Ok()
-                    .content_type("APPLICATION_JSON")
-                    .json(format!("Successsfully added row: {:?}", r)),
+            return generate_err_response(&mut HttpResponse::Ok(),format!("Successsfully added row: {:?}", r)),
         Err(e) => 
-            return HttpResponse::InternalServerError()
-                    .content_type("APPLICATION_JSON")
-                    .json(e.to_string()),
+            return generate_err_response(&mut HttpResponse::InternalServerError(),e.to_string()),
     };
 }
 
@@ -27,13 +24,9 @@ pub async fn index() -> String {
 pub async fn get_tables() -> impl Responder {
     match workers::get_tables(&DB_IP) {
         Ok(tables) => 
-            return HttpResponse::Ok()
-                .content_type("APPLICATION_JSON")
-                .json(tables),
+            return generate_err_response(&mut HttpResponse::Ok(),tables),
         Err(e) => 
-            return HttpResponse::InternalServerError()
-                .content_type("APPLICATION_JSON")
-                .json(e.to_string()),
+            return generate_err_response(&mut HttpResponse::InternalServerError(), e.to_string()),
     }
     
 }
@@ -43,13 +36,9 @@ pub async fn get_order(path: web::Path<String>) -> impl Responder {
     let id = path.into_inner();
     match workers::get_row(&id, &DB_IP) {
         Ok(r) => 
-            return HttpResponse::Ok()
-                .content_type("APPLICATION_JSON")
-                .json(format!("Successfully got order: {:?}", r.o_id)),
+            return generate_err_response(&mut HttpResponse::Ok(),format!("Successfully got order: {:?}", r.o_id)),
         Err(e) =>
-            return HttpResponse::InternalServerError()
-                .content_type("APPLICATION_JSON")
-                .json(e.to_string()),
+            return generate_err_response(&mut HttpResponse::InternalServerError(), e.to_string()),
     }
 }
 
@@ -59,12 +48,12 @@ pub async fn get_orders_from_user(path: web::Path<String>) -> impl Responder {
     println!("{id}");
     match workers::get_orders_info_by_user(&id, &DB_IP) {
         Ok(r) => 
-            return HttpResponse::Ok()
-                .content_type("APPLICATION_JSON")
-                .json(r),
+            return generate_err_response(&mut HttpResponse::Ok(), r),
         Err(e) =>
-        return HttpResponse::InternalServerError()
-            .content_type("APPLICATION_JSON")
-            .json(e.to_string()),
+            return generate_err_response(&mut HttpResponse::InternalServerError(), e.to_string()),
     }
+}
+
+fn generate_err_response(response_builder: &mut HttpResponseBuilder, error: impl Serialize) -> HttpResponse {
+    response_builder.content_type("APPLICATION_JSON").json(error)
 }
--- src/api/utils/env.rs
+++ src/api/utils/env.rs
@@ -1,4 +1,15 @@
+use std::env;
 
+pub fn get_env_var(var: &str) -> Option<String> {
+    match env::var(var) {
+        Ok(v) => Some(v),
+        Err(_) => None,
+    }
+}
+
+pub fn get_db_ip() -> Option<String> {
+    get_env_var("HBASE_IP")
+}
 
 #[cfg(test)]
 mod tests {
