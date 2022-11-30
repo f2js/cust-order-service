865eef7f35a11f1fa48861a19acd64d2a9761043
fdinsen <gammarik@gmail.com>
2022-11-30 12:27:46 +0100

    Adds first test for producer

--- src/api/producers.rs
+++ src/api/producers.rs
@@ -1,9 +1,17 @@
 //TODO: Refactor for environment variables
 const KAFKA_IP: &str = "10.245.211.251:9092";
 
-
+fn raise_event(topic_name: &str) -> Result<(), ()> {
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
+    #[test]
+    fn test_raise_event_is_ok() {
+        let res = raise_event("topic");
+        assert!(res.is_ok());
+    }
 }
